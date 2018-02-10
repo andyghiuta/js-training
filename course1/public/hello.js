1176ff99c2ff21c9640cff4ac74268dbfd440c50
Sergiu <sergiu.ojoc@gmail.com>
2018-02-10 11:30:54 +0200

    Succeeded with two if's conditions.

--- course1/public/hello.js
+++ course1/public/hello.js
@@ -1,8 +1,7 @@
 writeJavaOrJs = {
 	statementIf: function(){
 		for (var i =1; i<=100;i++){
-			var output = (i%3==0) && (i%5==0) ? "Javascript" : (i%3==0 ? "Java" + (i%5==0?"Script":""): i);
-			console.log(output)
+			console.log(i%3==0 ? "Java" + (i%5==0?"Script":""): i);
  		}
 	}
 }
