133f52903b86239d1995fdb51c1b611b1d252658
Sergiu <sergiu.ojoc@gmail.com>
2018-02-20 07:14:22 +0200

    Updating the scores!

--- course2/public/words.js
+++ course2/public/words.js
@@ -1,11 +1,11 @@
 let wordsToGuess = {
-	computers: {
+	/*computers: {
 		"the computer brand name":["dell", "ibm", "lenovo", "machintosh", "asus", "acer", "fujitsu", "toshiba", "hitachi", "sony", "msi", "hewlett packard", "alienware"]
-	},
+	},*/
 	games: {
-		"the game": ["counter strike", "Dark Souls", "vampyr", "Shadow of the Colossus", "Call of Duty", "Devil May Cry", "mario", "Final Fantasy", "BioShock", "Metal Gear Solid"],
-	},
+		"the game": ["counter strike"/*, "Dark Souls", "vampyr", "Shadow of the Colossus", "Call of Duty", "Devil May Cry", "mario", "Final Fantasy", "BioShock", "Metal Gear Solid"*/],
+	}/*,
 	foodIngredients:{
 		"the food ingredients":["onion", "tomatoes", "garlic", "pepper","salad", "cabbage", "ananas", "onion"]
-	}
+	}*/
 }
