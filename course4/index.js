7c4b68b94a254b8e72c3c6f11bab723a233a5c18
Andy Ghiuta <andy.ghiuta@gmail.com>
2018-03-09 10:52:48 +0200

    course 4 async/await

--- course4/index.js
+++ course4/index.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-// const util = require('util');
+const util = require('util');
 // Convert fs.readFile && fs.writeFile into Promise version of same
-// const readFile = util.promisify(fs.readFile);
-// const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const dbPath = path.join(__dirname, 'data', 'db.json');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,29 +23,38 @@ app.get('/data', function(req, res) {
   });
 });
 
-// POST "data" route handler
-app.post('/data', function(req, res) {
-  // read "db" file
-  fs.readFile(dbPath, 'utf8', function (err, data) {
+
+let doneWrite = function(req, res) {
+  return function (err) {
+    if (err) {
+      res.status(500).send('Couldn\'t write DB!');
+    }
+    res.json({
+      status: 'ok'
+    });
+  };
+};
+
+let doneRead = function(req, res) {
+  return function (err, data) {
     if (err) {
       res.status(500).send('Couldn\'t read DB!');
     }
     let json = JSON.parse(data);
     // add the shape
     json.push(req.body);
-    fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', function (err) {
-      if (err) {
-        res.status(500).send('Couldn\'t write DB!');
-      }
-      res.json({
-        status: 'ok'
-      });
-    });
-  });
+    fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', doneWrite(req, res));
+  }
+};
+
+// POST "data" route handler
+app.post('/data', function(req, res) {
+  // read "db" file
+  fs.readFile(dbPath, 'utf8', doneRead(req, res));
 });
 
 // async POST
-/*let readData = async function() {
+let readData = async function() {
   let data = await readFile(dbPath, 'utf8');
   return JSON.parse(data);
 };
@@ -59,20 +68,34 @@ let saveShape = async function(shape) {
   data.push(shape);
   await writeData(data);
   return true;
-}
+};
 
-// POST "data" route handler
+// // POST "data" route handler
 app.post('/data', function(req, res) {
-  saveShape(req.body)
-    .then(() => {
+  (async function () {
+    try {
+      await saveShape(req.body);
       res.json({
         status: 'ok'
       });
-    })
-    .catch((err) => {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
       res.status(500).send('Couldn\'t write to DB!');
-    });
-});*/
+    } finally {
+      // everytime
+    }
+  })();
+  // saveShape(req.body)
+  //   .then(() => {
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).send('Couldn\'t write to DB!');
+  //   }).
+    // finally(() => {
+
+    // });
+  console.log('11');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
